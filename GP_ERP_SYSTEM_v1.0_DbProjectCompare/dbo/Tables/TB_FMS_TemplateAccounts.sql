4ad2f0f57483df11a8559085b5da8eb2cc53f695
hussein <hussein@LAPTOP-U3MH7N9D>
2023-02-16 20:15:30 +0200

    adding composite primary keys to some fms tables

--- GP_ERP_SYSTEM_v1.0_DbProjectCompare/dbo/Tables/TB_FMS_TemplateAccounts.sql
+++ GP_ERP_SYSTEM_v1.0_DbProjectCompare/dbo/Tables/TB_FMS_TemplateAccounts.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[TB_FMS_TemplateAccounts] (
-    [AccID]  INT NULL,
-    [TempID] INT NULL,
-    [ID]     INT IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_TB_FMS_TemplateAccounts] PRIMARY KEY CLUSTERED ([ID] ASC),
+    [AccID]  INT NOT NULL,
+    [TempID] INT NOT NULL,
+    CONSTRAINT [composite_pk template_account] PRIMARY KEY CLUSTERED ([AccID] ASC, [TempID] ASC),
     CONSTRAINT [FK__TB_FMS_Te__AccID__160F4887] FOREIGN KEY ([AccID]) REFERENCES [dbo].[TB_FMS_Account] ([AccID]),
     CONSTRAINT [FK__TB_FMS_Te__TempI__17036CC0] FOREIGN KEY ([TempID]) REFERENCES [dbo].[TB_FMS_StatementTemplate] ([TempID])
 );
+
+
 
