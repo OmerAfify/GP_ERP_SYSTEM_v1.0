050602b06c6b47733b3fbcd20edf559b1f55022a
OmerAfify <omarafify3102001@gmail.com>
2023-06-08 10:27:54 +0300

    just a commit

--- GP_ERP_SYSTEM_v1.0/Controllers/ManufacturingController.cs
+++ GP_ERP_SYSTEM_v1.0/Controllers/ManufacturingController.cs
@@ -17,7 +17,7 @@ namespace GP_ERP_SYSTEM_v1._0
     [Route("api/[action]")]
     [ApiController]
 
-    [Authorize(Roles = "Admin,SCM")]
+   // [Authorize(Roles = "Admin,SCM")]
     public class ManufacturingController : ControllerBase
     {
         private readonly IManufacturingOrderService _manufacturingService;
